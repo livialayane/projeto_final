785c9d7b2e3805f579a51158d499bc1955343652
livialayane <liviabrlayane@gmail.com>
2024-01-09 21:42:47 -0300

    banco

--- backup.sql
+++ backup.sql
@@ -1,4 +1,4 @@
--- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
+- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
 --
 -- Host: localhost    Database: legalcodesolution
 -- ------------------------------------------------------
